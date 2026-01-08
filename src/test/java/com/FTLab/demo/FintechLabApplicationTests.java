7bc9ad7661970ebe4e4f5bbf3a3efc167195f9bc
aka2148 <ak2006feb7@gmail.com>
2026-01-08 15:45:18 +0530

    First commit

---
+++ src/test/java/com/FTLab/demo/FintechLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FTLab.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
